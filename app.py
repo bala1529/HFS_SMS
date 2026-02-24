21a4c2a8ee607e7dc3af65ef35ca09b4ce224064
bala1529 <bala.tm.43@gmail.com>
2026-02-24 22:48:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -237,4 +237,6 @@ def predict():
                            extracted_text=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
